ad76e97b5fca53609c6f1aeecc4f595d9b936f82
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2026-02-15 14:14:05 -0300

    fix: minor fixes

--- contact_site/views.py
+++ contact_site/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
+    
+    context = {
+        "site_title": "Home"
+    }
     return render(
         request,
-        "index.html",
+        "index.html", context
     )
