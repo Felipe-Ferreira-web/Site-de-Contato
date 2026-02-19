2e44ab3fefe37389464a0263cb4ca1546d82d86b
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2026-02-19 15:43:32 -0300

    feat: função para alternar layout da header

---
+++ contact_site/static/js/main.js
@@ -0,0 +1,17 @@
+// Função para mostrar opções da header em celulares
+function onToggleMenu(button) {
+    const navLinks = document.querySelector('#nav-menu');
+    const iconContainer = document.querySelector('#icon-container');
+
+    navLinks.classList.toggle('top-[5rem]');
+    
+    button.classList.add('scale-90');
+    setTimeout(() => button.classList.remove('scale-90'), 100);
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+        const navLinks = document.querySelector('#nav-menu');
+        navLinks.classList.remove('top-[5rem]');
+    }
+});
