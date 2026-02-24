4244f19ae6e5a0b445feacfeedd78deda6f1f943
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2026-02-23 23:03:14 -0300

    fix: remoção de elementos

--- contact_site/static/js/main.js
+++ contact_site/static/js/main.js
@@ -1,7 +1,6 @@
 // Função para mostrar opções da header em celulares
 function onToggleMenu(button) {
     const navLinks = document.querySelector('#nav-menu');
-    const iconContainer = document.querySelector('#icon-container');
 
     navLinks.classList.toggle('top-[5rem]');
     
@@ -15,17 +14,3 @@ window.addEventListener('resize', () => {
         navLinks.classList.remove('top-[5rem]');
     }
 });
-
-// Função para envio de formulário
-const btn = document.querySelector('#submitbtn')
-
-btn.addEventListener('click', function(event){
-    event.preventDefault()
-
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const object = document.querySelector('#object').value
-    const message = document.querySelector('#message').value
-
-    console.log(name)
-})
